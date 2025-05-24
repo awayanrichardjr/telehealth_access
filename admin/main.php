e19627a75772c86bf55ef417cc7ca0efed0817d5
Richard <awayanrichardjr@gmail.com>
2025-05-25 00:12:35 +0800

    Refactor dropdown menu: remove commented code and simplify link labels for clarity

--- admin/main.php
+++ admin/main.php
@@ -23,36 +23,21 @@
                     <ul class="dropdown-content">
                         <li>
                             <a href="../public/dashboard.php">
-                                <!-- <span class="material-symbols-outlined">group</span> -->
                                 User summary
                             </a>
                         </li>
                         <li>
                             <a href="../public/specialties.php">
-                                <!-- <span class="material-symbols-outlined">clinical_notes</span> -->
                                 Specialist summary
                             </a>
                         </li>
                         <li>
                             <a href="../public/statistic.php">
-                                <!-- <span class="material-symbols-outlined">clinical_notes</span> -->
-                                Predictive Analytics
+                                Analytics
                             </a>
                         </li>
-                        <!-- <li>
-                            <a href="../public/monthly_analytics.php">
-                                Mothly Analytics
-                            </a>
-                        </li> -->
                     </ul>
                 </li>
-
-                <!-- <li>
-                    <a href="../public/statistic.php">
-                        <span class="material-symbols-outlined">monitoring</span>
-                        <p>Statistics</p>
-                    </a>
-                </li> -->
 
                 <!-- dropdown -->
                 <div class="dropdown-links">
@@ -65,25 +50,21 @@
                     <div class="dropdown-content">
                         <li>
                             <a href="../public/records_doc.php">
-                                <!-- <span class="material-symbols-outlined">stethoscope</span> -->
                                 Doctors Records
                             </a>
                         </li>
                         <li>
                             <a href="../public/records_p.php">
-                                <!-- <span class="material-symbols-outlined">personal_injury</span> -->
                                 Patients Records
                             </a>
                         </li>
                         <li>
                             <a href="../public/appointment_admin.php">
-                                <!-- <span class="material-symbols-outlined">contract</span> -->
                                 Appointment Records
                             </a>
                         </li>
                         <li>
                             <a href="../public/medical_admin.php">
-                                <!-- <span class="material-symbols-outlined">clinical_notes</span> -->
                                 Medical Records
                             </a>
                         </li>
